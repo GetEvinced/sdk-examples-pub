0f95c9b91568f4f55100a22cb4f12bc0d912cc9b
Shane C <shane@evinced.com>
2026-05-01 09:52:08 +0100

    adding testmu example w wdio (#241)

---
+++ framework-examples/mobile/testmu-wdio/config/wdio.ios.real.conf.js
@@ -0,0 +1,30 @@
+require('dotenv').config({ path: '.env.local' });
+
+exports.config = {
+  user: process.env.LT_USERNAME,
+  key: process.env.LT_ACCESS_KEY,
+  hostname: 'mobile-hub.lambdatest.com',
+  port: 80,
+  path: '/wd/hub',
+
+  specs: ['../tests/evinced.test.js'],
+
+  capabilities: [{
+    platformName: 'iOS',
+    'appium:deviceName': 'iPhone 14',
+    'appium:platformVersion': '17',
+    'appium:app': process.env.LT_IOS_APP_URL,
+    'appium:autoAcceptAlerts': true,
+    'LT:Options': {
+      build: 'Evinced iOS Demo',
+      name: 'Evinced Accessibility Scan - iOS Real Device',
+      isRealMobile: true,
+      w3c: true,
+    },
+  }],
+
+  logLevel: 'warn',
+  framework: 'mocha',
+  reporters: ['spec'],
+  mochaOpts: { timeout: 120000 },
+};
