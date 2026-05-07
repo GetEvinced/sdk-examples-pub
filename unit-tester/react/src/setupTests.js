f2e3d9b0d0394cb8951e40843a9a3e3c625ca1fc
Shane C <shane@evinced.com>
2026-05-07 15:49:19 +0100

    repro for 2028

--- unit-tester/react/src/setupTests.js
+++ unit-tester/react/src/setupTests.js
@@ -10,7 +10,8 @@ import { configure } from "@evinced/unit-tester";
 
 configure({
   serviceAccountId: process.env.EVINCED_SERVICE_ID,
-  serviceAccountSecret: process.env.EVINCED_API_KEY,
+  // serviceAccountSecret: process.env.EVINCED_API_KEY,
+  offlineToken: process.env.EVINCED_WEB_OFFLINE_TOKEN
 });
 
 // configure({
